db18a42c39de902085d9325387321ccef5acc3af
mamajonoff <newmamajonoff@gmail.com>
2022-06-30 19:55:00 +0500

    V2.0

---
+++ MovieMVC/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieMVC.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
